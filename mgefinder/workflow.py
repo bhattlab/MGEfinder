782523155efc49ba15619976e12517ff3db0d37b
Matt Durrant <durrantmm@gmail.com>
2019-10-22 01:03:43 +0000

    improved execution of snakemake

--- mgefinder/workflow.py
+++ mgefinder/workflow.py
@@ -1,7 +1,19 @@
-from snakemake import snakemake
+from snakemake import shell
 
 
 def _workflow(workdir, snakefile, configfile, cores, memory, unlock, rerun_incomplete, keep_going):
 
-    snakemake(snakefile, configfile=configfile, config={'wd': workdir, 'memory': memory}, cores=cores, unlock=unlock,
-              force_incomplete=rerun_incomplete, keepgoing=keep_going)
+    if unlock == True:
+        cmd = 'snakemake -s {snakefile} --configfile {configfile} --config wd={workdir} --unlock'.format(
+              snakefile=snakefile, configfile=configfile, workdir=workdir
+        )
+    cmd = 'snakemake -s {snakefile} --config wd={workdir} memory={memory} ' \
+          '--cores {cores} --configfile {configfile}'
+    if rerun_incomplete:
+        cmd += '--rerun-incomplete '
+    if keep_going:
+        cmd += '--keep-going'
+    
+    cmd = cmd.format(snakefile=snakefile, configfile=configfile, workdir=workdir, memory=memory, cores=cores)
+    print('COMMAND:', cmd)
+    shell(cmd)
