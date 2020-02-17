448a9f6fb79e1ff08f7cde77dcdcd6487cb69b26
Matt George Durrant <mdurrant@smsh11dsu-srcf-d15-35.scg.stanford.edu>
2020-02-17 11:34:32 -0700

    just updating versions for new patch v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mgefinder",
-    version='1.0.0',
+    version='1.0.1',
     description='A toolbox for identifying mobile genetic element (MGE) insertions from short-read sequencing data of bacterial isolates.',
     url='https://github.com/bhattlab/MGEfinder',
     author="Matt Durrant",
